e6cba2f118de005a7ab4d228027ba8e6192f5ce6
Nicolas <davidnicolas431@gmail.com>
2024-02-16 16:24:39 -0500

    create new project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,51 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'EjercicioEnCasa';
+  nombre = 'Google';
+  url = 'https://www.google.com';
+  meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio'];
+  ventas = [10000, 20000, 30000, 40000, 50000, 60000];
+  tipo = 'Multinacional';
+
+  ventasPrimerSemestre() {
+    let suma=0;
+    for (let i = 0; i < this.ventas.length; i++) 
+      suma += this.ventas[i];
+    return suma;
+  }
+
+  mesMayorVentas() {
+    let indiceMayor = 0;
+    let ventasMayor = this.ventas[0];
+  
+    for (let i = 1; i < this.ventas.length; i++) {
+      if (this.ventas[i] > ventasMayor) {
+        ventasMayor = this.ventas[i];
+        indiceMayor = i;
+      }
+    }
+  
+    return { mes: this.meses[indiceMayor], ventas: ventasMayor };
+  }
+
+  mesMenorVentas() {
+    let indiceMayor = 0;
+    let ventasMenor = this.ventas[0];
+  
+    for (let i = 1; i < this.ventas.length; i++) {
+      if (this.ventas[i] < ventasMenor) {
+        ventasMenor = this.ventas[i];
+        indiceMayor = i;
+      }
+    }
+  
+    return { mes: this.meses[indiceMayor], ventas: ventasMenor };
+  }
+
+  PromedioVentasPrimerSemestre() {
+    let suma=0;
+    for (let i = 0; i < this.ventas.length; i++) 
+      suma += this.ventas[i];
+    return Math.round(suma/this.ventas.length);
+  }
 }
